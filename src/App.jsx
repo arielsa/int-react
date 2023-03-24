577ee71db355ec30dccc26e59c7ff2ca30b53503
ariel <santa.cruz.unla@gmail.com>
2023-03-24 01:41:16 -0300

    estructura basica de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from 'react';
+import React from 'react'
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import {TodoList} from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+
+const todos=[
+  {text:'aprender asp.net', completed: false},
+  {text:'aprender phyton', completed: false},
+  {text:'aprender django ', completed: false},
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h2>holis</h2>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+    <React.Fragment>
+
+      <TodoCounter/>
+      
+      <TodoSearch/>
+      <input placeholder='aprender JS en profundidad' />
+      
+      <TodoList>
+
+        {todos.map(item => (
+          <TodoItem key={item.text} text={item.text} />
+        ))  }
+
+      </TodoList>
+
+
+      <CreateTodoButton/>
+      
+
+    </React.Fragment>
+
+
+
+  );
 }
 
 export default App
---
+++ src/TodoCounter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TodoCounter(){
+    return(
+        <h2>has completado 2 de 3 tareas</h2>
+    );
+
+}
+
+export {TodoCounter};
